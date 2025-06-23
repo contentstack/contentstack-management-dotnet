340620b9474bc5405a698462432fb15d67eae53f
Stas Marchenko <stas.marchenko@nextleague.com>
2025-06-23 15:57:40 -0400

    fix: support ScheduledAt in PublishUnpublishDetails

--- Contentstack.Management.Core/Services/Models/PublishUnpublishService.cs
+++ Contentstack.Management.Core/Services/Models/PublishUnpublishService.cs
@@ -83,7 +83,7 @@ namespace Contentstack.Management.Core.Services.Models
                     writer.WriteValue(locale);
 
                 }
-                if (!string.IsNullOrEmpty(details.Version))
+                if (!string.IsNullOrEmpty(details.ScheduledAt))
                 {
                     writer.WritePropertyName("scheduled_at");
                     writer.WriteValue(details.ScheduledAt);
