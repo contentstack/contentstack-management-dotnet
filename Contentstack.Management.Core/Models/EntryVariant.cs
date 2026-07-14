8444c8dc5046e5cafde191b09db48914a04a4efc
OMpawar-21 <om.pawar@contentstack.com>
2026-07-14 15:24:02 +0530

    Revert "Potential fix for pull request finding"
    This reverts commit b23f36a02941fe80a0575eefe868c49943d54e59.

--- Contentstack.Management.Core/Models/EntryVariant.cs
+++ Contentstack.Management.Core/Models/EntryVariant.cs
@@ -284,9 +284,11 @@ namespace Contentstack.Management.Core.Models
                 throw new ArgumentNullException(nameof(details));
             }
 
+            details.Variants ??= new System.Collections.Generic.List<PublishVariant>();
+
             if (!details.Variants.Exists(v => v.Uid == this.Uid))
             {
-                details.Variants.Add(new PublishVariant { Uid = this.Uid });
+                details.Variants.Add(new PublishVariant { Uid = this.Uid, Version = details.Version });
             }
         }
 
